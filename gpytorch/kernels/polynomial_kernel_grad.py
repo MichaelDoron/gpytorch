a303019f50d7337553796f281cad1fa260cbe06e
dme65 <dme65@github.com>
2019-05-31 16:02:56 -0700

    WIP

--- gpytorch/kernels/polynomial_kernel_grad.py
+++ gpytorch/kernels/polynomial_kernel_grad.py
@@ -24,12 +24,6 @@ class PolynomialKernelGrad(PolynomialKernel):
         n1, d = x1.shape[-2:]
         n2 = x2.shape[-2]
 
-        K = torch.zeros(*batch_shape, n1 * (d + 1), n2 * (d + 1), device=x1.device, dtype=x1.dtype)
-
-        if last_dim_is_batch:
-            x1 = x1.transpose(-1, -2).unsqueeze(-1)
-            x2 = x2.transpose(-1, -2).unsqueeze(-1)
-
         if diag:
             raise RuntimeError("None done yet")
             K11 = ((x1 * x2).sum(dim=-1) + self.offset).pow(self.power)
@@ -37,29 +31,43 @@ class PolynomialKernelGrad(PolynomialKernel):
             base_inner_prod = torch.matmul(x1, x2.transpose(-2, -1)) + offset
             K11 = base_inner_prod.pow(self.power)
 
-            K21_base = (self.power * base_inner_prod.pow(self.power - 1))
+            K12_base = self.power * base_inner_prod.pow(self.power - 1)
+            K12 = torch.zeros((*batch_shape, n1, n2 * d))
+            for i in range(d):
+                K12[:, n2*i:n2*(i+1)] =  K12_base * torch.ger(x1[:, i], torch.ones(n2,))
 
-            K12 = (K21_base.unsqueeze(-2).transpose(-2, -1) * x1.unsqueeze(-2)).view(*batch_shape, n1, -1)
+            # K21_base = (self.power * base_inner_prod.pow(self.power - 1))
+            # K12 = (K21_base.unsqueeze(-2).transpose(-2, -1) * x1.unsqueeze(-2)).view(*batch_shape, n1, -1)
 
-            K21 = (K21_base.unsqueeze(-2).permute(-1, -3, -2) * x2.unsqueeze(-2))
-            K21 = K21.contiguous().view(*batch_shape, n2, -1).transpose(-2, -1)
+            K21 = torch.zeros((*batch_shape, n1 * d, n2))
+            for i in range(d):
+                K21[n1*i:n1*(i+1), :] = torch.ger(torch.ones(n1,), x2[:, i]) * K12_base
+            
+            # K21 = (K21_base.unsqueeze(-2).permute(-1, -3, -2) * x2.unsqueeze(-2))
+            # K21 = K21.contiguous().view(*batch_shape, n2, -1).transpose(-2, -1)
 
-            all_outers = x1.unsqueeze(-2).unsqueeze(-2).transpose(-2, -1).matmul(x2.unsqueeze(-3).unsqueeze(-2))
-            K22_base = base_inner_prod.pow(self.power - 2) * (self.power) * (self.power - 1)
-            K22 = (K22_base.unsqueeze(-1).unsqueeze(-1) * all_outers).view(n1 * d, n2 * d)
+            K22_base = self.power * (self.power - 1) * base_inner_prod.pow(self.power - 2)
+            K22 = torch.zeros(n1*d, n2*d)
+            for i in range(d):
+                for j in range(d):        
+                    K22[n1*i:n1*(i+1), n2*j:n2*(j+1)] = K22_base * torch.ger(x1[:, j], x2[:, i]) + (i == j) * K12_base
+            
+            # all_outers = x1.unsqueeze(-2).unsqueeze(-2).transpose(-2, -1).matmul(x2.unsqueeze(-3).unsqueeze(-2))
+            # K22_base = base_inner_prod.pow(self.power - 2) * (self.power) * (self.power - 1)
+            # K22 = (K22_base.unsqueeze(-1).unsqueeze(-1) * all_outers).transpose(-3, -1).contiguous().view(n1 * d, n2 * d)
 
-            kp = KroneckerProductLazyTensor(
-                torch.eye(d, d, device=x1.device, dtype=x1.dtype).repeat(*batch_shape, 1, 1),
-                K21_base,
-            )
-
-            K22 = K22 + kp.evaluate()
+            # kp = KroneckerProductLazyTensor(
+            #     torch.eye(d, d, device=x1.device, dtype=x1.dtype).repeat(*batch_shape, 1, 1),
+            #     K12_base,
+            # )
+            # K22 = K22 + kp.evaluate()
 
             K = torch.cat([torch.cat([K11, K12], dim=-1), torch.cat([K21, K22], dim=-1)])
 
-            # pi1 = torch.arange(n1 * (d + 1)).view(d + 1, n1).t().contiguous().view((n1 * (d + 1)))
-            # pi2 = torch.arange(n2 * (d + 1)).view(d + 1, n2).t().contiguous().view((n2 * (d + 1)))
-            # K = K[..., pi1, :][..., :, pi2]
+            # Apply perfect shuffle
+            pi1 = torch.arange(n1 * (d + 1)).view(d + 1, n1).t().contiguous().view((n1 * (d + 1)))
+            pi2 = torch.arange(n2 * (d + 1)).view(d + 1, n2).t().contiguous().view((n2 * (d + 1)))
+            K = K[..., pi1, :][..., :, pi2]
 
             return K
 
